acb6f653452a5894f47cc2cc19bffbba4d92173e
Joao Parreira <joao_parreira@mail.telepac.pt>
2024-06-23 13:23:45 +0100

    axuliar

---
+++ src/main/java/Emprestimo.java
@@ -0,0 +1,2 @@
+public class Emprestimo {
+}
---
+++ src/main/java/JanelaConfiguracoes.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class JanelaConfiguracoes {
+    private JPanel panel1;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JTextField textField4;
+    private JButton voltarButton;
+    private JButton submeterButton;
+}
