d3839d57470d400c28c632fef04580a10545a25c
Diogo Abegão <2222184@my.ipleiria.pt>
2024-06-24 22:46:25 +0100

    janelas socios

---
+++ src/main/java/JanelaSocios.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class JanelaSocios {
+    private JPanel JanelaSocios;
+    private JButton btnAdicionarSocio;
+    private JButton btnPagamentos;
+    private JButton btnHistoricoSocio;
+    private JButton btnNotificarSocio;
+    private JButton btnVoltar;
+    private JCheckBox btnFiltrarSociosDividas;
+    private JTable tableListaSocios;
+}
