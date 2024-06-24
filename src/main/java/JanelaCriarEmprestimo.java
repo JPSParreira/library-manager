c3caa9bb662d2e4e8de9aacf1d74bd36858f321b
Joao Parreira <joao_parreira@mail.telepac.pt>
2024-06-24 22:17:21 +0100

    fixed

--- src/main/java/JanelaCriarEmprestimo.java
+++ src/main/java/JanelaCriarEmprestimo.java
@@ -18,17 +18,17 @@ public class JanelaCriarEmprestimo extends JDialog {
         pack();
         setLocationRelativeTo(null);
 
-        DefaultComboBoxModel<Titulo> modelTitulo = new DefaultComboBoxModel<>();
-        for (Titulo titulo : GestorBiblioteca.instance.getTitulos()) {
-            modelTitulo.addElement(titulo);
-        }
-        comboBoxTitulo.setModel(modelTitulo);
-
-        DefaultComboBoxModel<Socio> modelSocio = new DefaultComboBoxModel<>();
-        for (Socio socio : GestorBiblioteca.instance.getSocios()) {
-            modelSocio.addElement(socio);
-        }
-        comboBoxSocio.setModel(modelSocio);
+//        DefaultComboBoxModel<Titulo> modelTitulo = new DefaultComboBoxModel<>();
+//        for (Titulo titulo : GestorBiblioteca.instance.getTitulos()) {
+//            modelTitulo.addElement(titulo);
+//        }
+//        comboBoxTitulo.setModel(modelTitulo);
+//
+//        DefaultComboBoxModel<Socio> modelSocio = new DefaultComboBoxModel<>();
+//        for (Socio socio : GestorBiblioteca.instance.getSocios()) {
+//            modelSocio.addElement(socio);
+//        }
+//        comboBoxSocio.setModel(modelSocio);
 
         submeterButton.addActionListener(this::submeterButtonActionPerformed);
         voltarButton.addActionListener(this::voltarButtonActionPerformed);
